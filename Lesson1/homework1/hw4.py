5d908d9910d82fe885d34f8cc9367ba642163ae8
Mirard <flynya@gmail.com>
2020-11-09 22:09:12 +0300

    Added 2nd version of 4th task (wo lists)

--- Lesson1/homework1/hw4.py
+++ Lesson1/homework1/hw4.py
@@ -1,3 +1,4 @@
+# Вариант 1
 num = -1
 num_list = []
 
@@ -15,3 +16,21 @@ num_list.append(part_num)
 num_list.sort()
 num_list.reverse()
 print("Наибольшая цифра в числе: ", num_list[0])
+
+# Вариант 2
+num = -1
+max_num = -1
+while num <= 0:
+    num = int(input("Введите целое положительное число >>> "))
+
+while (num // 10) != 0:
+    part_num = num % 10
+    if part_num > max_num:
+        max_num = part_num
+    num = num // 10
+# Добавляем первую цифру в список
+part_num = num % 10
+if part_num > max_num:
+    max_num = part_num
+
+print("Наибольшая цифра в числе: ", max_num)
